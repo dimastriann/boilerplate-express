b6a63ad4237bb17e64fd2a6e42feae82757b86db
Dimas Nugraha <43321064+dimastriann@users.noreply.github.com>
2022-02-17 09:53:24 +0000

    some changes in myApp.js

--- myApp.js
+++ myApp.js
@@ -1,5 +1,63 @@
 var express = require('express');
+const bodyParser = require('body-parser');
 var app = express();
+
+console.log("Hello World");
+app.use(bodyParser.urlencoded({extended: false}))
+app.use('/public', express.static(__dirname + '/public'))
+
+app.use('/', (req, res, next) => {
+  console.log(`${req.method} /${req.path} - ${req.ip}`)
+  next();
+})
+
+app.get('/', (req, res) => {
+  // res.send("Hello Express");
+  res.sendFile(__dirname + '/views/index.html')
+})
+
+app.get("/json", (req, res) => {
+  let response = "Hello json";
+  if(process.env.MESSAGE_STYLE == "uppercase"){
+    response = response.toUpperCase();
+  }
+  
+  res.json({
+    "message": response
+  })
+})
+
+app.get('/now', (req, res, next) => {
+  req.time = new Date().toString();
+  next();
+}, (req, res) => {
+  res.send({
+    time: req.time
+  });
+})
+
+app.get('/:word/echo', (req, res) => {
+  const { word } = req.params;
+  res.json({
+    echo: word
+  })
+})
+
+app.route('/name')
+  .get( (req, res) => {
+    const { first, last } = req.query;
+    // console.log(req.query);
+    res.json({
+      name: `${first} ${last}`
+    })
+  })
+  .post( (req, res) => {
+    const { first, last } = req.body;
+    // console.log(req.body);
+    res.json({
+      name: `${first} ${last}`
+    }) 
+  })
 
 
 
